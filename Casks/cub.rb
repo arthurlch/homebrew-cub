e6c18bf8515b915ee664337141f8f55d29bc10ce
art <43084242+arthurlch@users.noreply.github.com>
2024-09-22 17:20:43 +0900

    Create cub.rb

---
+++ Casks/cub.rb
@@ -0,0 +1,19 @@
+# homebrew-cub/Casks/cub.rb
+
+cask "cub" do
+  version "0.1.0"
+  sha256 "YOUR_INITIAL_SHA256_HASH"
+
+  url "https://github.com/arthurlch/cub/releases/download/v#{version}/cub-binaries-v#{version}.zip"
+  name "Cub"
+  desc "A simple and powerful text editor."
+  homepage "https://github.com/arthurlch/cub"
+
+  binary "cub"
+
+  binary "cub_macos-intel"
+  binary "cub_macos-arm"
+  binary "cub_windows.exe"
+  binary "cub_linux"
+
+end
